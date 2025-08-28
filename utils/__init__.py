f8b21a7912acc295e395a84d6b761dee9e3cb55e
Ezio Camporeale <ezio@Mac-mini-di-Ezio.local>
2025-08-28 13:45:57 +0100

    Fix import structure: correct package imports and remove problematic __init__.py files

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,13 @@
 # Utils package
+from .backup import BackupManager
+from .helpers import format_currency, format_date, validate_email
+from .logger import setup_logger, get_logger
+
+__all__ = [
+    'BackupManager',
+    'format_currency',
+    'format_date', 
+    'validate_email',
+    'setup_logger',
+    'get_logger'
+]
